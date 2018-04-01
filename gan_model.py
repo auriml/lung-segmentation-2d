41abad683d2983f0a823e183c4124ed4758a37f3
Aurelia Bustos <aureliabustos@gmail.com>
2018-04-01 09:49:51 +0200

    Simplified epochs

--- gan_model.py
+++ gan_model.py
@@ -355,8 +355,8 @@ def dice_coef_loss(y_true, y_pred):
 if __name__ == '__main__':
     gan = GAN()
     #gan.train(epochs=30000, batch_size=32, save_interval=200)
-    #gan.train(epochs=1000, batch_size=20, save_interval=25)
-    gan.train(epochs=100, batch_size=4, save_interval=25)
+    gan.train(epochs=1000, batch_size=20, save_interval=25)
+    #gan.train(epochs=100, batch_size=4, save_interval=25)
 
 
 
